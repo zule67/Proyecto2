1b4d3ac74c0a9bf7ddcce9388789b7e17f88eba6
carlos <carlos@DESKTOP-J61L43O.mshome.net>
2021-07-25 17:45:20 +0200

    Primer commit

---
+++ src/test/java/com/example/EventTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/example/service/EventNotificationServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.Attendee;
+import com.example.Event;
+import com.example.EventType;
+
+class EventNotificationServiceImplTest {
+	
+}
