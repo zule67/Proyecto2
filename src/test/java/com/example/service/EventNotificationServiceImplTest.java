78cf776c130a08944f41295fb2aa3e1316095201
carlos <carlos@DESKTOP-J61L43O.mshome.net>
2021-07-25 17:56:43 +0200

    Segundo commit de EventNotificationService

--- src/test/java/com/example/service/EventNotificationServiceImplTest.java
+++ src/test/java/com/example/service/EventNotificationServiceImplTest.java
@@ -12,5 +12,69 @@ import com.example.Event;
 import com.example.EventType;
 
 class EventNotificationServiceImplTest {
-	
+	EventNotificationServiceImpl service ;
+	Event event;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		service = new EventNotificationServiceImpl();
+		event = new Event(1L, "", EventType.MARKETING, service);
+	}
+
+	@Test
+	void testNulos() {
+
+		Attendee asistente1 = new Attendee();
+		event.addAttendee(asistente1);
+		
+		//
+		assertEquals(0, asistente1.getNotifications().size());
+		service.announce(null);
+		assertEquals(0, asistente1.getNotifications().size());
+
+		// 
+		event.setAttendees(null);
+		service.announce(event);
+		assertEquals(0, asistente1.getNotifications().size());
+
+		// 
+		event.setAttendees(new ArrayList<Attendee>());
+		service.announce(event);
+		assertEquals(0, asistente1.getNotifications().size());
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
